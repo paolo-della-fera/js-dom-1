cd71d12f730013bb3b8315e2f1c10df70565794b
paolo-della-fera <paolodf2005@gmail.com>
2026-02-11 15:54:06 +0100

    add variable to call html elements

---
+++ main.js
@@ -0,0 +1,4 @@
+// inizio scrivendomi le variabili per richiamare gli elementi dall'html
+const ligthOff = document.getElementById('ligth_off')
+const ligthOn = document.getElementById('ligth_on')
+const btn = document.getElementById('btn')
