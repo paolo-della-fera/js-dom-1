5eef96a038de83922e74253334d7844f4bd38add
paolo-della-fera <paolodf2005@gmail.com>
2026-02-11 17:50:12 +0100

    add click to button and logic to turn the light on and off

--- main.js
+++ main.js
@@ -4,4 +4,21 @@ const ligthOn = document.getElementById('ligth_on')
 const btn = document.getElementById('btn')
 
 // creo una variabile che mi dice lo stato della lampadina
-let turnedOn = false
+let turnedOn = false;
+
+// aggiungo il click del bottone per accendere e spegnere la luce 
+btn.addEventListener('click', function() {
+    turnedOn = !turnedOn; 
+    // SE clicco il bottone e la lampadina è spenta disattivo l'immagine della lampadina spenta e attivo l'immagine della lampadina accesa 
+    if (turnedOn) { 
+        ligthOff.style.display = 'none';
+        ligthOn.style.display = 'block';
+    }
+    //ALTRIMENTI eseguo il contrario per spegnerla
+    else {
+        ligthOff.style.display = 'block';
+        ligthOn.style.display = 'none';
+    }
+
+    console.log('click btn');
+}) 
