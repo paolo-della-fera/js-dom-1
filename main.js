be0d40f0f20e3d6eec8b6419955c77d288b9a7c5
paolo-della-fera <paolodf2005@gmail.com>
2026-02-11 16:00:12 +0100

    add bulb state variable

--- main.js
+++ main.js
@@ -2,3 +2,6 @@
 const ligthOff = document.getElementById('ligth_off')
 const ligthOn = document.getElementById('ligth_on')
 const btn = document.getElementById('btn')
+
+// creo una variabile che mi dice lo stato della lampadina
+let turnedOn = false
